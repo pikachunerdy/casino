b9c00639641d474b01915fb9b5bd00284851aa54
hardcommitoneself <hardcommitoneself@gmail.com>
2022-10-09 22:34:51 -0400

    Init repository

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import styled from 'styled-components'
+
+const Home: NextPage = () => {
+  return (
+    <Wrapper>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div.attrs({ className: 'flex flex-col' })``
+
+export default Home
