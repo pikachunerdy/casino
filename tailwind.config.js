be31923dae219cafac405a7860997dd17f3313d8
hardcommitoneself <hardcommitoneself@gmail.com>
2022-10-11 11:45:13 -0400

    Added resources

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#1F1F1E",
+        darkBlue: "#0492C2",
+        darkWhite: "rgba(255, 255, 255, 0.5);",
+        white1: "#C9C9C9",
+      },
+    },
   },
   plugins: [],
-}
+};
