22047d4cf8f95afa43779ed2b129efabc0e9524e
hardcommitoneself <hardcommitoneself@gmail.com>
2022-10-13 08:50:36 -0400

    feat/featured Added home featuredcasino component

--- components/Home/FeaturedCasino.js
+++ components/Home/FeaturedCasino.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
-import { FeatureTitle } from "./FeaturedCasino.module";
+import { FeatureTitle, ContentTitle } from "./FeaturedCasino.module";
+import Rating from "../core/Rating/Rating";
+import { FaArrowRight } from "react-icons/fa";
+import Button from "../core/Button/Button";
 
 const FeaturedCasino = (props) => {
   return (
@@ -13,6 +16,25 @@ const FeaturedCasino = (props) => {
             80vw"
       ></Image>
       <FeatureTitle>{props.title}</FeatureTitle>
+      <div className="flex">
+        <Rating value={props.value} activeColor="#C9C9C9"></Rating>
+        <ContentTitle className="pl-5">
+          {props.value}/5 based on {props.review}reviews
+        </ContentTitle>
+      </div>
+      <div className="pt-3">
+        <Button
+          name="Visit Casino"
+          handleClick={() => {
+            console.log("Visit Casino");
+          }}
+          variant="model"
+        >
+          <div className="pl-1 pt-1">
+            <FaArrowRight />
+          </div>
+        </Button>
+      </div>
     </>
   );
 };
--- components/Home/FeaturedCasino.module.js
+++ components/Home/FeaturedCasino.module.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
-export const FeatureTitle = styled.div.attrs({
+export const FeatureTitle = styled.h3.attrs({
   className: "text-white1 font-semibold text-[33px]",
+})``;
+
+export const ContentTitle = styled.p.attrs({
+  className: "text-white font-normal text-[18px] leading-9",
 })``;
