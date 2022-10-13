f6530bc3e9b806548f224a408d00fbda6addf6fd
hardcommitoneself <hardcommitoneself@gmail.com>
2022-10-13 08:53:03 -0400

    feat/carousel Added home page carousel component

---
+++ components/Home/Carousel.js
@@ -0,0 +1,92 @@
+import { useEffect } from "react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { FreeMode, Pagination, Scrollbar, A11y } from "swiper";
+import Button from "../core/Button/Button";
+import HomeCard from "./HomeCard";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const Carousel = (props) => {
+  const swiperHandle = useSwiper();
+  console.log("props.cards: ", props.cards);
+  return (
+    <div>
+      <Swiper
+        modules={[Scrollbar, A11y]}
+        spaceBetween={10}
+        slidesPerView={3}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          1200: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1600: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => {
+          swiperHandle = swiper;
+        }}
+        className="gap-4"
+      >
+        {props.cards.map((card) => {
+          return (
+            <SwiperSlide>
+              <HomeCard
+                title={card.title}
+                value={card.value}
+                content={card.content}
+                avatar={card.avatar}
+                user={card.name}
+              ></HomeCard>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <div className="flex justify-center mt-14 gap-3">
+        <Button
+          name=""
+          handleClick={() => {
+            if (!!swiperHandle) {
+              swiperHandle.slidePrev();
+            }
+          }}
+          variant="model"
+        >
+          <FontAwesomeIcon icon={faChevronLeft} size="x" />
+        </Button>
+
+        <Button
+          name=""
+          handleClick={() => {
+            if (!!swiperHandle) {
+              swiperHandle.slideNext();
+            }
+          }}
+          variant="model"
+        >
+          <FontAwesomeIcon icon={faChevronRight} size="x" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
