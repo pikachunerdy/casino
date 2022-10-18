6b413e886124a393e985295f00530b5dea88404b
hardcommitoneself <hardcommitoneself@gmail.com>
2022-10-18 11:15:02 -0400

    fix/button Updated button component style

--- components/core/Button/Button.module.js
+++ components/core/Button/Button.module.js
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const CustomButton = styled.button.attrs((props) => ({
   className: `${
-    props.variant == "model" ? "bg-darkBlue" : ""
+    props.variant == "model" ? "bg-blue1" : ""
   } py-2 px-5 rounded-3xl`,
 }))``;
