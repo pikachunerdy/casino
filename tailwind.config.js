87347e2ebaebea680ccfd79d23a6184ba1db6540
hardcommitoneself <hardcommitoneself@gmail.com>
2022-10-18 11:14:36 -0400

    chore/resource Update resources

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Clash: "Clash Display",
+      },
       colors: {
         dark: "#121212",
         dark1: "#363636",
@@ -16,6 +19,7 @@ module.exports = {
         black1: "#1F1F1E",
         blue1: "#0492C2",
         blue2: "#66BEED",
+        blue3: "#1676F8",
         green1: "#00FF95",
         purple: "#976AFF",
         pink: "#CC4D93",
