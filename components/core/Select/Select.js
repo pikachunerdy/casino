be97a8ea35f30f4c5f9e5965b6990b562100099e
hardcommitoneself <hardcommitoneself@gmail.com>
2022-10-31 12:05:36 -0400

    fix/select Updated select component UI

--- components/core/Select/Select.js
+++ components/core/Select/Select.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import { FaChevronDown } from "react-icons/fa";
 
 const Select = (props) => {
   return (
     <>
       <Dropdown
         className="relative"
-        controlClassName="bg-[#1f1f1e] border-solid border border-[#3d3d3d] rounded-md py-3 px-4"
-        placeholderClassName="opacity-60 placeholder:text-[#667085]"
-        menuClassName="mt-2 bg-[#1f1f1e] border-solid border border-[#3d3d3d] rounded-md py-3 px-4 absolute w-full"
+        controlClassName="!bg-[#1f1f1e] !text-white1 !border-solid !border !border-[#3d3d3d] !rounded-md !py-3 !px-4"
+        placeholderClassName="!opacity-60 !placeholder:text-[#667085]"
+        menuClassName="!mt-2 !bg-[#1f1f1e] !border-solid !border !border-[#3d3d3d] !rounded-md !py-3 !px-4 !absolute !w-full"
+        arrowClassName="!mt-[6px]"
+        arrowOpen={<FaChevronDown />}
         options={props.options}
         placeholder={props.placeholder}
       />
