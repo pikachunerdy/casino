2cee5aac4b4536c16b94ed5e2369155db1e9665f
fattycrocodile <fullbravery@gmail.com>
2022-11-29 17:55:27 +0200

    small button colour change

--- components/core/Button/Button.module.js
+++ components/core/Button/Button.module.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const CustomButton = styled.button.attrs((props) => ({
   className: `${
     props.variant == "model"
-      ? "dark:bg-blue3 bg-transparent text-black border border-black dark:hover:bg-blue2 hover:bg-blue3 hover:border-white"
+      ? "dark:bg-blue3 bg-transparent text-black hover:text-white border border-black dark:hover:bg-blue2 hover:bg-blue2 hover:border-white"
       : "border dark:hover:bg-blue2 hover:bg-blue2 dark:bg-transparent bg-blue3 hover:border-transparent"
   } w-fit py-2 px-3 md:py-2 md:px-6 rounded-3xl dark:text-white text-white ${
     props.className
